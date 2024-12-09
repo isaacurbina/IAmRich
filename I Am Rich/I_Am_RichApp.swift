a99e235af23ab75e19002c525ba0d35514c275c0
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2024-12-08 20:47:25 -0600

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Isaac Urbina on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
